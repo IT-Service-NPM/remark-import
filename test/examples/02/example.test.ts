5d25bfbca1fa8103824846c48cf785ad7e95653f
sergey-s-betke <sergey.s.betke@yandex.ru>
2026-02-15 23:24:35 +0300

    test: fail if the file attribute is missing

--- test/examples/02/example.test.ts
+++ test/examples/02/example.test.ts
@@ -7,22 +7,24 @@ const testSnapshotsFilesPath: string = path.join(__dirname, 'snapshots');
 
 describe('remark-include', () => {
 
-  it('must include single markdown file', async () => {
-    const _cwd = process.cwd();
-    try {
-      process.chdir(__dirname);
+  it('must include sub-documents with file name without extension',
+    async () => {
+      const _cwd = process.cwd();
+      try {
+        process.chdir(__dirname);
 
-      const outputFile = await remarkDirectiveUsingExample(
-        path.join(testSrcFilesPath, 'main.md')
-      );
+        const outputFile = await remarkDirectiveUsingExample(
+          path.join(testSrcFilesPath, 'main.md')
+        );
 
-      await expect(String(outputFile))
-        .toMatchFileSnapshot(path.join(testSnapshotsFilesPath, 'output.md'));
+        await expect(String(outputFile))
+          .toMatchFileSnapshot(path.join(testSnapshotsFilesPath, 'output.md'));
 
-    } finally {
-      process.chdir(_cwd);
-    };
+      } finally {
+        process.chdir(_cwd);
+      };
 
-  });
+    }
+  );
 
 });
